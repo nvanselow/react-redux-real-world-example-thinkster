cde558870f6e0a9debcd8946de9f7cf7c2310c13
Nick(Mac) <nvanselow@hotmail.com>
2016-11-20 13:20:28 -0500

    Add http calls for auth

--- src/services/agent.js
+++ src/services/agent.js
@@ -8,12 +8,20 @@ const responseBody = res => res.body;
 
 const requests = {
   get: url => superagent.get(`${API_ROOT}${url}`).then(responseBody),
+  post: (url, body) =>
+    superagent.post(`${API_ROOT}${url}`, body).then(responseBody),
 };
 
 const Articles = {
   all: page => requests.get('/articles?limit=10'),
 };
 
+const Auth = {
+  login: (email, password) =>
+    requests.post('/users/login', { user: { email, password } }),
+};
+
 export default {
   Articles,
+  Auth,
 };
